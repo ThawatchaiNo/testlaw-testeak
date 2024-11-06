67c79b3d45dd35780609011ef28de8323d672a27
thawatchai.no <thawatchai.no@inet.co.th>
2024-11-06 14:43:07 +0700

    go to github

---
+++ test.js
@@ -0,0 +1,17 @@
+console.log('helo 55')
+
+let a = 5
+let b = 10
+
+let number = a + b
+console.log('sum is ', number)
+document.getElementById("sum").innerHTML = a + b;
+
+function showMessage () {
+    let msg = "Wowwwwww";
+    alert(msg);
+}
+
+let x = 3.14;
+let y = 3;
+document.getElementById("demo").innerHTML = x + "<br>" + y;
